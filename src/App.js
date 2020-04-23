3c043a098d2cd2fe69499141728792f1ef92de83
Deivis <8516648+Lisviks@users.noreply.github.com>
2020-04-23 05:24:40 +0100

    switch theme and save to local storage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import Todos from './components/Todos';
 import ThemeSwitch from './components/ThemeSwitch';
 
 class App extends Component {
+  state = {
+    darkTheme: true,
+  };
+
+  componentDidMount() {
+    const darkTheme = JSON.parse(localStorage.getItem('theme'));
+    this.setState({ darkTheme });
+
+    if (this.state.darkTheme) {
+      document.querySelector('body').classList = 'dark';
+    } else {
+      document.querySelector('body').classList = 'light';
+    }
+  }
+
+  switchTheme = () => {
+    this.setState({ darkTheme: !this.state.darkTheme }, () =>
+      localStorage.setItem('theme', JSON.stringify(this.state.darkTheme))
+    );
+
+    document.querySelector('body').classList.toggle('light');
+    document.querySelector('body').classList.toggle('dark');
+  };
+
+  currentTheme = () => {
+    return this.state.darkTheme;
+  };
+
   render() {
     return (
       <div className='container'>
-        <ThemeSwitch />
+        <ThemeSwitch
+          switchTheme={this.switchTheme}
+          currentTheme={this.currentTheme}
+        />
         <h1>Todo App</h1>
         <Todos />
       </div>
--- src/components/ThemeSwitch.js
+++ src/components/ThemeSwitch.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const ThemeSwitch = () => {
+const ThemeSwitch = ({ switchTheme, currentTheme }) => {
   return (
     <div className='theme-switch'>
       <span>Light</span>
       <label className='switch'>
-        <input type='checkbox' />
+        <input
+          type='checkbox'
+          onChange={switchTheme}
+          checked={currentTheme()}
+        />
         <span className='slider'></span>
       </label>
       <span>Dark</span>
