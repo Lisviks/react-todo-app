6bdb8852b19a196a988818d7b31970279813e7fb
Deivis <8516648+Lisviks@users.noreply.github.com>
2020-04-25 18:06:25 +0100

    initial redux setup

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import todosReducer from './todosReducer';
+import authReducer from './authReducer';
+
+export default combineReducers({
+  todos: todosReducer,
+  auth: authReducer,
+});
